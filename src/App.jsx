958c89caa4f3650456e1e47cc3b70104720e3ecb
vishalicious213 <vishalicious213@gmail.com>
2023-08-18 15:26:15 -0700

    set up App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,12 @@ import ReactDOM from "react-dom/client"
 
 function App() {
     console.log("React Buttons")
+
+    return (
+        <main>
+            MAIN
+        </main>
+    )
 }
 
 ReactDOM.createRoot(document.getElementById("root")).render(<App />)
