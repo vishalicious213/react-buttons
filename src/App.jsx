88ddd7f8ff869cca302b8058574d77657491a29d
vishalicious213 <vishalicious213@gmail.com>
2023-08-21 13:53:14 -0700

    added faMoneyBill tp App

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import Button from "../components/Button"
 import ButtonChildren from "../components/Button-Children"
+import { FaMoneyBill } from "react-icons/fa"
 
 export default function App() {
     return (
         <main>
             <Button text="Click here" />
-            <ButtonChildren>Buy Now!</ButtonChildren>
+            <ButtonChildren><FaMoneyBill /> Buy Now!</ButtonChildren>
         </main>
     )
 }
