4ba5a761f8501dc27f32b82f82033032d7a43654
vishalicious213 <vishalicious213@gmail.com>
2023-08-21 14:05:39 -0700

    added FcGoogle to App. rendering 'log in with google' button

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Button from "../components/Button"
 import ButtonChildren from "../components/Button-Children"
 import { FaMoneyBill } from "react-icons/fa"
+import { FcGoogle } from "react-icons/fc"
 
 export default function App() {
     return (
         <main>
             <Button text="Click here" />
             <ButtonChildren><FaMoneyBill /> Buy Now!</ButtonChildren>
+            <ButtonChildren><FcGoogle /> Log in with Google</ButtonChildren>
         </main>
     )
 }
