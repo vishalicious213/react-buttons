1fa2274d615b545713d59a15f9c728a6d8faa522
vishalicious213 <vishalicious213@gmail.com>
2023-08-24 00:19:28 -0700

    drafted /components/Menu.jsx from Scrimba code

---
+++ components/Menu.jsx
@@ -0,0 +1,21 @@
+import MenuButton from "./MenuButton"
+import MenuDropdown from "./MenuDropdown"
+
+export default function Menu({ buttonText = "Menu", items }) {
+    const [open, setOpen] = React.useState(true)
+
+    function toggle() {
+        setOpen(prevOpen => !prevOpen)
+    }
+
+    return (
+        <div className="menu">
+            <MenuButton
+                buttonText={buttonText}
+                onClick={toggle}
+            />
+
+            {open && <MenuDropdown items={items} />}
+        </div>
+    )
+}
