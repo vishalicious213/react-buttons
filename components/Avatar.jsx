d1b7a0d0a592ca14db88762b0409c23eb7f33a7a
vishalicious213 <vishalicious213@gmail.com>
2023-08-23 17:38:09 -0700

    updated /components/Avatar.jsx

--- components/Avatar.jsx
+++ components/Avatar.jsx
@@ -2,10 +2,18 @@ import { IoPersonSharp } from "react-icons/io5"
 
 export default function Avatar(props) {
     if (props.src) {
-        return (<img className="avatar" src={props.src} alt={props.alt} />)
+        return (
+            <div className="avatar">
+                <img src={props.src} alt={props.alt} />
+            </div>
+        )
     } else if (props.children) {
-        return (<h2 className="avatar avatar-letters">{props.children}</h2>)
-    } else return (<h2 className="avatar avatar-icon"><IoPersonSharp /></h2>)
+        return (
+            <div className="avatar avatar-letters">{props.children}</div>
+        )
+    } else return (
+        <div className="avatar avatar-icon"><IoPersonSharp /></div>
+        )
 }
 
 // * With image: `avatar`
