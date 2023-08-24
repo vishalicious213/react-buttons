679cdacb0e1308196cbcab34c9aae48003973686
vishalicious213 <vishalicious213@gmail.com>
2023-08-24 00:06:59 -0700

    drafted /components/MenuButton.jsx from Scrimba code

---
+++ components/MenuButton.jsx
@@ -0,0 +1,7 @@
+import Button from "../Button/Button"
+
+export default function MenuButton({ buttonText, onClick }) {
+    return (
+        <Button onClick={onClick}>{buttonText}</Button>
+    )
+}
