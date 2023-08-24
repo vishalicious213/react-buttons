a946cdf2e3e204d01695b3bd66e08c3d7eabb567
vishalicious213 <vishalicious213@gmail.com>
2023-08-23 17:21:00 -0700

    added CSS variables. applied styles to Avatar.jsx

--- components/Avatar.jsx
+++ components/Avatar.jsx
@@ -4,8 +4,8 @@ export default function Avatar(props) {
     if (props.src) {
         return (<img className="avatar" src={props.src} alt={props.alt} />)
     } else if (props.children) {
-        return (<h2 className="avatar">{props.children}</h2>)
-    } else return (<h2 className="avatar"><IoPersonSharp /></h2>)
+        return (<h2 className="avatar avatar-letters">{props.children}</h2>)
+    } else return (<h2 className="avatar avatar-icon"><IoPersonSharp /></h2>)
 }
 
 // * With image: `avatar`
