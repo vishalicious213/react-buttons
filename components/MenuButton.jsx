6bbe536e3ff74e0a1a78c28474f1622b69d27c86
vishalicious213 <vishalicious213@gmail.com>
2023-08-29 13:49:57 -0700

    MenuButton uses { toggle } from MenuContext

--- components/MenuButton.jsx
+++ components/MenuButton.jsx
@@ -1,6 +1,10 @@
 import Button from "./Button"
+import { useContext } from "react"
+import { MenuContext } from "./Menu"
 
-export default function MenuButton({ toggle, children }) {
+export default function MenuButton({ children }) {
+    const { toggle } = useContext(MenuContext)
+    
     return (
         <Button onClick={toggle}>{children}</Button>
     )
