c70b4d41c5bf3c8f938c9a61e8cb6f9c5c4c29e7
vishalicious213 <vishalicious213@gmail.com>
2023-08-29 13:47:57 -0700

    MenuDropdown uses { open } from MenuContext

--- components/MenuDropdown.jsx
+++ components/MenuDropdown.jsx
@@ -2,7 +2,7 @@ import { useContext } from "react"
 import { MenuContext } from "./Menu"
 
 export default function MenuDropdown({ children }) {
-    const value = useContext(MenuContext)
+    const { open } = useContext(MenuContext)
 
-    return value ? (<div className="menu-dropdown">{children}</div>) : null
+    return open ? (<div className="menu-dropdown">{children}</div>) : null
 }
