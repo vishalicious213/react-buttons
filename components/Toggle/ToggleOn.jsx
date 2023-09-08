f10396772f8fa6cca1755084fedf24bc858a6a24
vishalicious213 <vishalicious213@gmail.com>
2023-09-07 23:19:16 -0700

    updated /components/Toggle/ToggleOn.jsx to use ToggleContext

--- components/Toggle/ToggleOn.jsx
+++ components/Toggle/ToggleOn.jsx
@@ -1,6 +1,10 @@
 import React from "react"
+import { useContext } from "react"
+import { ToggleContext } from "./Toggle"
 
 export default function ToggleOn({ children }) {
+    const { on } = useContext(ToggleContext)
+
     return (
         <div></div>
     )
