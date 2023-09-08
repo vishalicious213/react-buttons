949166f82ddc28c6eafcdc0ba24ef6a76335f798
vishalicious213 <vishalicious213@gmail.com>
2023-09-07 19:11:34 -0700

    updated /components/Toggle/Toggle.Button.jsx to use ToggleContext

--- components/Toggle/Toggle.Button.jsx
+++ components/Toggle/Toggle.Button.jsx
@@ -1,8 +1,13 @@
 import React from "react"
 import { useContext } from "react"
+import { ToggleContext } from "./Toggle"
 
 export default function Button() {
+    const value = useContext(ToggleContext)
+
     return (
-        <div></div>
+        <div onClick={value.toggle}>
+            { children }
+        </div>
     )
 }
