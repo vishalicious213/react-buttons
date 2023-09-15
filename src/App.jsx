25e74c9847cda47bee344824f3734793b263966a
vishalicious213 <vishalicious213@gmail.com>
2023-09-15 15:28:06 -0700

    cleaned up App

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,16 @@
-import Button from "../components/Button"
-import ButtonChildren from "../components/Button-Children"
-import Avatar from "../components/Avatar"
 import Menu from "../components/Menu/index"
+import CompoundStar from "../components/StarCompound"
+// import Button from "../components/Button"
+// import ButtonChildren from "../components/Button-Children"
+// import Avatar from "../components/Avatar"
 // import MenuButton from "../components/Menu/MenuButton"
 // import MenuDropdown from "../components/Menu/MenuDropdown"
 // import MenuItem from "../components/Menu/MenuItem"
 // import Star from "../components/Star"
 // import Toggle from "../components/Toggle/index"
-import { BsStar, BsStarFill } from "react-icons/bs"
-import CompoundStar from "../components/StarCompound"
-
-import { FaMoneyBill } from "react-icons/fa"
-import { FcGoogle } from "react-icons/fc"
+// import { BsStar, BsStarFill } from "react-icons/bs"
+// import { FaMoneyBill } from "react-icons/fa"
+// import { FcGoogle } from "react-icons/fc"
 
 export default function App() {
     const sports = ["Tennis", "Pickleball", "Racquetball", "Squash"]
@@ -22,6 +21,15 @@ export default function App() {
 
     return (
         <main>
+            <CompoundStar />
+
+            <Menu>
+                <Menu.Button>Sports</Menu.Button>
+                <Menu.Dropdown>
+                    {sports.map(sport => <Menu.Item key={sport}>{sport}</Menu.Item>)}
+                </Menu.Dropdown>
+            </Menu>
+
             {/* <Button text="Click here" />
 
             <ButtonChildren>
@@ -70,15 +78,6 @@ export default function App() {
                     </Toggle.Off>
                 </Toggle.Button>
             </Toggle> */}
-
-            <CompoundStar />
-
-            <Menu>
-                <Menu.Button>Sports</Menu.Button>
-                <Menu.Dropdown>
-                    {sports.map(sport => <Menu.Item key={sport}>{sport}</Menu.Item>)}
-                </Menu.Dropdown>
-            </Menu>
 
             {/* <Menu>
                 <Menu.Button>Sports</Menu.Button>
