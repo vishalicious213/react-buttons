6d246db7b3dee91bc1e109402d96dfbf2d0786fa
vishalicious213 <vishalicious213@gmail.com>
2023-09-15 16:16:55 -0700

    updated Toggle to take in an onToggle prop. added useEffect that runs when 'on' is changed

--- components/Toggle/Toggle.jsx
+++ components/Toggle/Toggle.jsx
@@ -1,12 +1,16 @@
 // This component shares state and a state setter function through context
 
 import React from "react"
-import { useState, createContext } from "react"
+import { useState, useEffect, createContext } from "react"
 
 const ToggleContext = createContext()
 
-export default function Toggle({ children}) {
+export default function Toggle({ children, onToggle }) {
     const [on, setOn] = useState(false)
+
+    useEffect(() => {
+        console.log("on", on)
+    }, [on])
 
     function toggle() {
         setOn(prevOn => !prevOn)
