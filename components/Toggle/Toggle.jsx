4510faaa2dba7892701ae9ee68cb72dcfe6636f0
vishalicious213 <vishalicious213@gmail.com>
2023-09-21 11:53:26 -0700

    updated Toggle with useRef and added firstRender ref

--- components/Toggle/Toggle.jsx
+++ components/Toggle/Toggle.jsx
@@ -1,12 +1,13 @@
 // This component shares state and a state setter function through context
 
 import React from "react"
-import { useState, useEffect, createContext } from "react"
+import { useState, useEffect, useRef, createContext } from "react"
 
 const ToggleContext = createContext()
 
 export default function Toggle({ children, onToggle }) {
     const [on, setOn] = useState(false)
+    const firstRender = useRef(true)
 
     useEffect(() => {
         if (typeof onToggle === "function") {
