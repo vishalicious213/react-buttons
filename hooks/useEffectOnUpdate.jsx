f5d8d1dbb6be690c8c6e26609521ee3b685a3415
vishalicious213 <vishalicious213@gmail.com>
2023-09-28 15:11:04 -0700

    updated /hooks/useEffectOnUpdate.jsx

--- hooks/useEffectOnUpdate.jsx
+++ hooks/useEffectOnUpdate.jsx
@@ -1,5 +1,13 @@
 import React from "react"
 
-function useEffectOnUpdate(effectFunction, deps) {
-    
+export default function useEffectOnUpdate(effectFunction, deps) {
+    const firstRender = React.useRef(true)
+
+    React.useEffect(() => {
+        if (firstRender.current) {
+            firstRender.current = false
+        } else {
+            effectFunction()
+        }
+    }, deps)
 }
