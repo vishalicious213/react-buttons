020155e6f15a81d1dd0899f8547ef2c1a1ddc334
vishalicious213 <vishalicious213@gmail.com>
2023-09-29 12:30:19 -0700

    updated Toggle with useToggle

--- components/Toggle/Toggle.jsx
+++ components/Toggle/Toggle.jsx
@@ -1,12 +1,14 @@
 // This component shares state and a state setter function through context
 
 import React from "react"
-import { useState, createContext } from "react"
+import { createContext } from "react"
 import useEffectOnUpdate from "../../hooks/useEffectOnUpdate"
+import useToggle from "../../hooks/useToggle"
 
 const ToggleContext = createContext()
 
 export default function Toggle({ children, onToggle }) {
+    const [on, toggle] = useToggle()
 
     useEffectOnUpdate(onToggle, [on])
 
