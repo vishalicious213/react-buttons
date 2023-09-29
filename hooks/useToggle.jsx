9b8963675c416da3cadc0e080540c9e678ff58f9
vishalicious213 <vishalicious213@gmail.com>
2023-09-28 19:28:03 -0700

    updated /hooks/useToggle.jsx

--- hooks/useToggle.jsx
+++ hooks/useToggle.jsx
@@ -1,5 +1,10 @@
 import React from "react"
+import { useState } from "react"
 
 export default function useToggle() {
-    
+    const [on, setOn] = useState(false)
+
+    function toggle() {
+        setOn(prevOn => !prevOn)
+    }
 }
