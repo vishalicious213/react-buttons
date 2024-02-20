efee3548a5b0aeef542bcae32a7809d806e5bc41
vishalicious213 <vishalicious213@gmail.com>
2024-02-20 11:13:03 -0800

    /components/StarCompound.jsx: updated to use useToggle()

--- components/StarCompound.jsx
+++ components/StarCompound.jsx
@@ -1,18 +1,26 @@
 import React from "react"
-import Toggle from "./Toggle"
+// import Toggle from "./Toggle"
 import { BsStar, BsStarFill } from "react-icons/bs"
+import useToggle from "../hooks/useToggle"
 
 export default function CompoundStar({ onChange }) {
+    const [on, toggle] = useToggle()
+    
     return (
-        <Toggle onToggle={onChange}>
-            <Toggle.Button>
-                <Toggle.On>
-                    <BsStarFill className="star filled" />
-                </Toggle.On>
-                <Toggle.Off>
-                    <BsStar className="star " />
-                </Toggle.Off>
-            </Toggle.Button>
-        </Toggle>
+        <BsStar 
+            className={on ? "star filled" : "star"} 
+            onClick={toggle}
+        />
+
+        // <Toggle onToggle={onChange}>
+        //     <Toggle.Button>
+        //         <Toggle.On>
+        //             <BsStarFill className="star filled" />
+        //         </Toggle.On>
+        //         <Toggle.Off>
+        //             <BsStar className="star " />
+        //         </Toggle.Off>
+        //     </Toggle.Button>
+        // </Toggle>
     )
 }
