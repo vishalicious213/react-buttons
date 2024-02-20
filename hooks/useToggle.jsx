8865e02ceb037d0f65501423cff3f60dcd736d85
vishalicious213 <vishalicious213@gmail.com>
2024-02-20 12:45:10 -0800

    /hooks/useToggle: passing in onToggle and setting it to a noop function

--- hooks/useToggle.jsx
+++ hooks/useToggle.jsx
@@ -1,7 +1,7 @@
 import React from "react"
 import { useState } from "react"
 
-export default function useToggle(initialValue=false) {
+export default function useToggle(initialValue=false, onToggle = () => {}) {
     const [on, setOn] = useState(initialValue)
 
     function toggle() {
