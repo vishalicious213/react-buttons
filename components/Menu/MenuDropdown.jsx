abd8ed0f9bec1a31337175bbd6ec9cdb8dd097a9
vishalicious213 <vishalicious213@gmail.com>
2024-02-20 11:44:57 -0800

    /components/MenuDropdown.jsx: removed Toggle logic and brought in useToggle()

--- components/Menu/MenuDropdown.jsx
+++ components/Menu/MenuDropdown.jsx
@@ -7,12 +7,16 @@
 //     return open ? (<div className="menu-dropdown">{children}</div>) : null
 // }
 
-import Toggle from "../Toggle"
+// import Toggle from "../Toggle"
+import useToggle from "../../hooks/useToggle"
 
 export default function MenuDropdown({ children }) {
+    const [open, toggleOpen] = useToggle()
+
     return (
-        <Toggle.On>
-            <div className="menu-dropdown">{children}</div>
-        </Toggle.On>
+        <div className="menu-dropdown"></div>
+        // <Toggle.On>
+        //     <div className="menu-dropdown">{children}</div>
+        // </Toggle.On>
     )
 }
